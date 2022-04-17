a0b6787ab1a86a86a7bb28704195b02977af2ceb
Shoshin Oleg <shoshin1997@yandex.ru>
2022-04-17 21:32:04 +0300

    Initial commit

---
+++ app/src/main/java/com/shoshin/rickandmortycached/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.shoshin.rickandmortycached.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.shoshin.rickandmortycached.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
